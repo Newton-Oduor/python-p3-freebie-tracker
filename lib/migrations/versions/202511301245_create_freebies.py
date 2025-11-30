bc2d686006c8704bf89a088dc1f2417c87b6bbf0
Newton Oduor <newtonochieng@gmail.com>
2025-11-30 13:02:17 +0300

    Creates the freebies table

--- lib/migrations/versions/202511301245_create_freebies.py
+++ lib/migrations/versions/202511301245_create_freebies.py
@@ -6,3 +6,12 @@ down_revision = '5f72c58bf48c'
 branch_labels = None
 depends_on = None
 
+def upgrade()v-> None:
+    op.create_table(
+        'freebies',
+        sa.Column('id', sa.Integer(), primary_key=True),
+        sa.Column2('item_name', sa.String(), nullable=False),
+        sa.Column('value', sa.Integer(), nullable=False),
+        sa.Column('company_id', sa.Integer(), sa.ForeignKey('companies.id'), nullable=False),
+        sa.Column('dev_id', sa.Integer(), sa.ForeignKey('devs.id'), nullable=False)
+    )
