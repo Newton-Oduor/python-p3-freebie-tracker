42364ab6b9e6dd401902bdcdffcaa49ca0cf2d49
Newton Oduor <newtonochieng@gmail.com>
2025-11-30 13:40:04 +0300

    Fixes issues on the create_company and create_freebies.py files and creates freebies.db

--- lib/migrations/versions/202511301245_create_freebies.py
+++ lib/migrations/versions/202511301245_create_freebies.py
@@ -10,7 +10,7 @@ def upgrade()-> None:
     op.create_table(
         'freebies',
         sa.Column('id', sa.Integer(), primary_key=True),
-        sa.Column2('item_name', sa.String(), nullable=False),
+        sa.Column('item_name', sa.String(), nullable=False),
         sa.Column('value', sa.Integer(), nullable=False),
         sa.Column('company_id', sa.Integer(), sa.ForeignKey('companies.id'), nullable=False),
         sa.Column('dev_id', sa.Integer(), sa.ForeignKey('devs.id'), nullable=False)
